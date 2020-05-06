ae41246e6c27e4b9b26278544880afe78d6b0cb1
Corey <corey@localhost.localdomain>
2020-05-06 01:09:47 -0400

    began lstm

---
+++ keras_wrapper_fix.py
@@ -0,0 +1,60 @@
+#A custom wrapper for Keras's scikit-learn wrapper class. Function is taken as is
+# from https://github.com/keras-team/keras/blob/master/keras/wrappers/scikit_learn.py
+# but the fit function has  been customized to include an argument to reset the
+# object's states.
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import copy
+import types
+
+import numpy as np
+
+from tensorflow.python.keras import losses
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.utils.generic_utils import has_arg
+from tensorflow.python.keras.utils.np_utils import to_categorical
+from tensorflow.python.util.tf_export import keras_export
+
+
+from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
+
+
+class Keras_Custom_Fitter(KerasRegressor):
+    def fit(self, x, y, **kwargs):
+        """Constructs a new model with `build_fn` & fit the model to `(x, y)`.
+        # Arguments
+            x : array-like, shape `(n_samples, n_features)`
+                Training samples where `n_samples` is the number of samples
+                and `n_features` is the number of features.
+            y : array-like, shape `(n_samples,)` or `(n_samples, n_outputs)`
+                True labels for `x`.
+            **kwargs: dictionary arguments
+                Legal arguments are the arguments of `Sequential.fit`
+        # Returns
+            history : object
+                details about the training history at each epoch.
+        """
+        if self.build_fn is None:
+            self.model = self.__call__(**self.filter_sk_params(self.__call__))
+        elif (not isinstance(self.build_fn, types.FunctionType) and
+              not isinstance(self.build_fn, types.MethodType)):
+            self.model = self.build_fn(
+                **self.filter_sk_params(self.build_fn.__call__))
+        else:
+            self.model = self.build_fn(**self.filter_sk_params(self.build_fn))
+
+        if (losses.is_categorical_crossentropy(self.model.loss) and
+                len(y.shape) != 2):
+            y = to_categorical(y)
+
+        fit_args = copy.deepcopy(self.filter_sk_params(Sequential.fit))
+        fit_args.update(kwargs)
+
+        history = self.model.fit(x, y, **fit_args)
+        history.reset_states()
+
+        return history
--- modeling_functions.py
+++ modeling_functions.py
@@ -3,7 +3,43 @@ import itertools
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+from sklearn.base import BaseEstimator, TransformerMixin
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
 
+
+class LSTM_Reshaper(BaseEstimator, TransformerMixin):
+
+    def __init__(self, variables=None):
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        return X.reshape(X.shape[0], 1, X.shape[1])
+
+
+def keras_model_wrapper():
+    model = Sequential()
+    model.add(LSTM(4, batch_input_shape=(X.shape[0], 1, X.shape[1]), stateful=True))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model
+
+def diff_handle_missing(df, diff, lag=0):
+    return df.diff(diff).iloc[diff + lag:]
+
+def ts_to_supervised(df, lag=1):
+    df = pd.DataFrame(df)
+    idx, shift = df.index, df.index.shift(lag)
+    new_idx = idx.union(shift)
+    return df.reindex(new_idx).shift(lag, fill_value=0)
 
 def pdq_combinations(season_period, max_p, max_d, max_q):
     p, d, q = range(0, max_p+1), range(0, max_d+1), range(0, max_q+1)
@@ -12,6 +48,8 @@ def pdq_combinations(season_period, max_p, max_d, max_q):
     return pdq, seasonal_pdq
 
 def pdq_hyperparam_search(df, season_period, max_p=1, max_d=1, max_q=1):
+    """Generates and searches a list of pdq and season values to find best hyperparameters."""
+
     pdq, seasonal_pdq = pdq_combinations(season_period, max_p, max_d, max_q)
     all_results=[]
     for param in pdq:
@@ -24,4 +62,23 @@ def pdq_hyperparam_search(df, season_period, max_p=1, max_d=1, max_q=1):
                 all_results.append([param, param_seasonal, rmse])
             except:
                 continue
+    return pd.DataFrame(all_results, columns = ["PDQ", "S-PDQ", "Error"]).sort_values("Error", ascending=True).reset_index(drop=True)
+
+def pdq_hyperparam_df_search(df, pdq_params, new_period=None, n_combs=5):
+    """Like pdq_hyperparam search, but iterates through a specific amount of values in a DataFrame. Used to check values if initial data needed resampling
+    for speed."""
+
+    all_results=[]
+    for i in range(0, n_combs):
+        param, param_seasonal = pdq_params.iloc[i:i+1, 0].to_list()[0], pdq_params.iloc[i:i+1, 1].to_list()[0]
+        if new_period:
+            param_seasonal = list(param_seasonal[0:3])+[new_period]
+        try:
+            mod = sm.tsa.statespace.SARIMAX(df, order=param, seasonal_order=param_seasonal, enforce_stationarity=False, enforce_invertibility=False)
+            results = mod.fit()
+            rmse = math.sqrt(results.mse)
+            print('ARIMA{}x{}12 - rmse:{}'.format(param, param_seasonal, rmse))
+            all_results.append([param, param_seasonal, rmse])
+        except:
+            continue
     return pd.DataFrame(all_results, columns = ["PDQ", "S-PDQ", "Error"]).sort_values("Error", ascending=True).reset_index(drop=True)
